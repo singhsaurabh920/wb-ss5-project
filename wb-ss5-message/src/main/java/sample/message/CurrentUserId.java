2cf6e3b6c0bdad7348c4d233f30b8ecd1e6de0a3
Saurabh Singh <sighsaurabh920@gmail.com>
2019-11-30 09:43:55 +0000

    Initial commit

---
+++ wb-ss5-message/src/main/java/sample/message/CurrentUserId.java
@@ -0,0 +1,12 @@
+package sample.message;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author Rob Winch
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentUserId {
+}
